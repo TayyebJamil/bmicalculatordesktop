3460d8565efc02e10d777404609441b8cbad080e
TayyebJamil <its.tayyeb.ch159@gmail.com>
2022-04-12 16:13:08 +0500

    lec56

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class InputPage extends StatefulWidget {
   @override
@@ -11,11 +12,52 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF1D1E33),
+                  borderRadius: BorderRadiusDirectional.circular(10.0)
+                ),
+              ),),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadiusDirectional.circular(10.0)
+                ),
+              ),),
+            ],
+          ),),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadiusDirectional.circular(10.0)
+            ),
+          ),),
+          Expanded(child: Row(
+          children: <Widget>[
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadiusDirectional.circular(10.0)
+            ),
+          ),),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadiusDirectional.circular(10.0)
+            ),
+          ),),
+        ],
+      ),),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,9 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-        textTheme: TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-        ),
       ),
     );
   }
