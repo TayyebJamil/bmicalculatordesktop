dd8bdbcf10f0e90e5a3312d2b639a1a54e517b02
TayyebJamil <its.tayyeb.ch159@gmail.com>
2022-04-13 15:27:06 +0500

    lec67/part13

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,6 +19,7 @@ class _InputPageState extends State<InputPage> {
   Gender selectGender;
   int sliderHeight=180;
   int sliderWeight=60;
+  int sliderAge=20;
 //  Color maleColor = deActiveColor;
   //Color feMaleColor = deActiveColor;
   //void updateColor(Gender gendertype)
@@ -132,6 +133,24 @@ class _InputPageState extends State<InputPage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
+                    RoundIcon(
+                      iconData: FontAwesomeIcons.minus,
+                      onPress: (){
+                        setState(() {
+                          sliderWeight--;
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10.0,),
+                    RoundIcon(
+                      iconData: FontAwesomeIcons.minus,
+                      onPress: (){
+                        setState(() {
+                          sliderWeight++;
+                        });
+                      },
+
+                    ),
 
                   ],
                 ),
@@ -141,11 +160,79 @@ class _InputPageState extends State<InputPage> {
           ),),
           Expanded(child: RepeatcontainerCode(
             colors: Color(0xFF1D1E33),
+            cardwidget: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Age',
+                  style: kLabelStyle,
+
+                ),
+                Text(
+                  sliderAge.toString(),
+                  style: kNumberStyle,
+
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RoundIcon(
+                      iconData: FontAwesomeIcons.minus,
+                      onPress: (){
+                        setState(() {
+                          sliderAge--;
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10.0,),
+                    RoundIcon(
+                      iconData: FontAwesomeIcons.minus,
+                      onPress: (){
+                        setState(() {
+                          sliderAge++;
+                        });
+                      },
+
+                    ),
+
+                  ],
+                ),
+
+              ],
+            ),
           ),),
+
         ],
       ),),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+          ),
         ],
       ),
+    );
+  }
+}
+class RoundIcon extends StatelessWidget {
+ RoundIcon({@required this.iconData,@required this.onPress});
+  final IconData iconData;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(iconData),
+      onPressed: onPress,
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        height: 56.0,
+        width: 56.0,
+
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
